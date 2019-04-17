003be6dcfbc50b0a6c03987f2d77ea391c5c9579
JaronX <alexander.nikolov092@gmail.com>
2019-04-17 22:20:29 +0200

    02 Exercises

---
+++ PythonBasics/02_Area_square.py
@@ -0,0 +1,4 @@
+side_a = int(input())
+area = side_a * side_a
+
+print(area)
---
+++ PythonBasics/02_Projects_Creation.py
@@ -0,0 +1,5 @@
+architekt_name = input()
+number_projects = int(input())
+needed_hours = number_projects * 3
+
+print(f'The architect {architekt_name} will need {needed_hours} hours to complete {number_projects} project/s.')
