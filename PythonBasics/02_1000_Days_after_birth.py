8671f29b705848a4af9a8177fa23f5baacdad093
JaronX <alexander.nikolov092@gmail.com>
2019-04-25 11:37:22 +0200

    02 Exercises #book

--- PythonBasics/02_1000_Days_after_birth.py
+++ PythonBasics/02_1000_Days_after_birth.py
@@ -1,18 +1,12 @@
 import datetime
 
 birthday_one = input()
-birthday_two = input()
+
 
 datetime_object = datetime.datetime.strptime(birthday_one, '%d-%m-%Y').date()
-modified_date = datetime_object + datetime.timedelta(days=999)
+modified_date = datetime_object + datetime.timedelta(days=1000)
 
-x = modified_date.strftime("%d-%m-%Y")
+date = modified_date.strftime("%d-%m-%Y")
 
 
-datetime_object2 = datetime.datetime.strptime(birthday_two, '%d-%m-%Y').date()
-modified_date2 = datetime_object2 + datetime.timedelta(days=999)
-
-x2 = modified_date2.strftime("%d-%m-%Y")
-
-print(x)
-print(x2)
+print(date)
